e136b2235c95204ca2504452a1f3d68045012c5c
sithdimalka <sithmidimalka1@gmail.com>
2026-08-31 23:49:08 +0530

    Add files via upload

---
+++ groups.js
@@ -0,0 +1,18 @@
+let groups = [
+  {
+    id: 1,
+    name: "Frontend Team",
+    description: "Members working on the CollabBoard frontend.",
+    createdBy: 1,
+    memberIds: [1, 2, 3, 5, 6],
+  },
+  {
+    id: 2,
+    name: "Backend Team",
+    description: "Members working on the CollabBoard backend.",
+    createdBy: 1,
+    memberIds: [1, 4, 6],
+  },
+];
+
+export default groups;
