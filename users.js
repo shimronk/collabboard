fef6d6e330ac9a8eec8acf4032bf4bda63035ee3
Sandewni Perera <yehangaperera@gmail.com>
2026-08-31 20:45:36 +0530

    Add files via upload

---
+++ users.js
@@ -0,0 +1,52 @@
+let users = [
+  {
+    id: 1,
+    name: "Kasun Perera",
+    email: "kasun@collabboard.com",
+    role: "Team Lead",
+    password: "$2b$10$fiQp0Gi7txF9GkaTMnP4sOq./6ZsPwzAJBmxdJfpyvnh/GWCCiXt.",
+    image: null,
+  },
+  {
+    id: 2,
+    name: "Nethmi Silva",
+    email: "nethmi@collabboard.com",
+    role: "Team Member",
+    password: null,
+    image: null,
+  },
+  {
+    id: 3,
+    name: "Sachini Fernando",
+    email: "sachini@collabboard.com",
+    role: "Developer",
+    password: null,
+    image: null,
+  },
+  {
+    id: 4,
+    name: "Amila Perera",
+    email: "amila@collabboard.com",
+    role: "Tester",
+    password: null,
+    image: null,
+  },
+  {
+    id: 5,
+    name: "Tharindu Silva",
+    email: "tharindu@collabboard.com",
+    role: "UI/UX Designer",
+    password: null,
+    image: null,
+  },
+  {
+    id: 6,
+    name: "Andrew Jansen",
+    email: "andrew@test.com",
+    role: "Developer",
+    password: "$2b$10$xIvCkR2vfr11v.HdQ5gL.ufjfRaZycfpaot5qWLkxHbVQCnW20J0i",
+    image: null,
+  },
+];
+
+export default users;
